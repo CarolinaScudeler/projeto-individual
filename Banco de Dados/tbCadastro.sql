1400d97d67123af4d3fd4940f87129552e474a9a
CarolinaScudeler <carolina.oliveira@bandtec.com.br>
2020-11-22 14:05:14 -0300

    att documentação e banco de dados

--- Banco de Dados/tbCadastro.sql
+++ Banco de Dados/tbCadastro.sql
@@ -1,15 +1,13 @@
-create database projeto;
-use projeto;
+create database gamer_community;
+use gamer_community;
 
 create table tbCadastro (
-idCadastro int primary key auto_increment,
-nomeCadastro varchar(100) not null,
-usuário varchar(20) not null,
+idUsuário int primary key auto_increment,
+nome varchar(100) not null,
+email varchar(20) not null,
 senha varchar (10) not null,
-celular int,
 estado varchar(20),
 cidade varchar(30)
 );
 
 insert into tbCadastro values
-(null, 'Carolina Scudeler', 'carolscudeler', 'Carol123', '11954491685', 'São Paulo', 'Osasco', null);
